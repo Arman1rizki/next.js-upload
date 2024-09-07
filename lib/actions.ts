68914cfa4b8ecdc3627f00a25f5afab014326fa9
arman1rizki <arman1rizki01@gmail.com>
2024-09-07 08:11:50 +0100

    mengubah tsx ke ts

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,12 +1,15 @@
 "use server";
 import { string, z } from "zod";
+import {prisma} from '../lib/prisma'
+import { put } from "@vercel/blob";
+import { revalidatePath } from "next/cache";
 
 const UploadSchema = z.object({
   title: z.string().min(1),
   image: z
     .instanceof(File)
     .refine((file) => file.size > 0, { message: "Image is required" })
-    .refine((file) => file.type.startsWith("image/"), {
+    .refine((file) => file.size === 0 || file.type.startsWith("image/"), {
       message: "Only image are allowed",
     })
     .refine((file) => file.size < 4000000, {
@@ -22,4 +25,23 @@ export const uploadImage = async (prevState: unknown, formData: FormData) => {
         error: validatedFields.error.flatten().fieldErrors
     }
   }
+
+  const {title, image} = validatedFields.data
+  const {url} = await put(image.name, image,{
+    access: 'public',
+    multipart: true
+  })
+
+  try {
+    await prisma.upload.create({
+      data:{
+        title,
+        image: url
+      }
+    })
+  } catch(error) {
+    return {message: 'Failed to create data'}
+  }
+  revalidatePath('/');
+  redirect('/');
 };
